b28255a20f4508c84dc5c641ea8960693576fcab
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-08-07 16:42:36 +0200

    init

---
+++ scripts/app.js
@@ -0,0 +1,119 @@
+(function() {
+    var global = { snipping: false };
+    window.global = global;
+ 
+    global.config = window.configService.load();
+    global.version = global.config.version;
+
+    document.title += " - " + global.version;
+
+    var wssend = function(obj) { 
+        var data = typeof(obj) != "object" ? obj : JSON.stringify(obj);
+        global.ws.send(data); 
+    };
+
+    function confirmAndSendToServer(msg, callback) {
+        vex.dialog.confirm({
+            message: msg,
+            callback: function(value) {
+                if(value) callback();
+            }
+        });
+    }
+
+    $(function() {
+        var sortBy = localStorage.getItem("sortPokemonBy") || "cp";
+        $("#sortBy" + sortBy).addClass("active").siblings().removeClass("active");
+
+        $("#pokemonLink").click( function() {
+            if ($(".inventory").css("opacity") == "1" && $(".inventory .data .pokemon").length) {
+                $(".inventory").removeClass("active");
+            } else {
+                wssend("PokemonList");
+                //ga("send", "event", "pokemonlist");
+            }
+        });
+        $("#eggsLink").click( function() {
+            if ($(".inventory").css("opacity") == "1" && $(".inventory .data .eggs").length) {
+                $(".inventory").removeClass("active");
+            } else { 
+                wssend("EggsList");
+                //ga("send", "event", "eggslist");
+            }
+        });
+        $("#inventoryLink").click( function() {
+            if ($(".inventory").css("opacity") == "1" && $(".inventory .data .items").length) {
+                $(".inventory").removeClass("active");
+            } else {
+                wssend("InventoryList");
+                //ga("send", "event", "inventorylist");
+            }
+        });
+
+        $("#sortBypokemonId").click(() => global.map.displayPokemonList(null, "pokemonId"));
+        $("#sortBycp").click(() => global.map.displayPokemonList(null, "cp"));
+        $("#sortByiv").click(() => global.map.displayPokemonList(null, "iv"));
+
+        $("#sortBypokemonId, #sortBycp, #sortByiv").click( function() {
+            if(!$(this).hasClass("active")) {
+                $(this).toggleClass("active").siblings().removeClass("active");
+            }
+        });
+
+        $(".inventory .close").click(function() {
+            $(this).parent().removeClass("active");
+            $(".inventory .sort").hide();
+        });
+
+        $(".message .close").click(function() {
+            $(this).parent().hide();
+        });
+
+        $("#recycleLink").click(() => {
+            sessionStorage.setItem("available", false);
+            window.location.reload();
+        });
+
+        $("#settingsLink").click(() => {
+            global.map.saveContext();
+            window.location = "config.html";
+        });
+
+        $(".inventory .data").on("click", "a.transferAction", function() {
+            var transfer = $(this).parent();
+            confirmAndSendToServer("Are you sure you want to transfer this Pokemon?", () => {
+                ga("send", "event", "transfer");
+                wssend({
+                    Command: "TransferPokemon",
+                    PokemonId: transfer.attr("id"),
+                    Data: transfer.attr("id")
+                });
+                transfer.parent().fadeOut();
+            });
+        });
+
+        $(".inventory .data").on("click", "a.evolveAction", function() {
+            var evolve = $(this).parent();
+            confirmAndSendToServer("Are you sure you want to evolve this Pokemon?", () => {
+                ga("send", "event", "transfer");
+                wssend({
+                    Command: "EvolvePokemon",
+                    PokemonId: evolve.attr("id"),
+                    Data: evolve.attr("id")
+                });
+                $(".inventory").removeClass("active");
+            });
+        });
+
+        if (global.config.websocket) {
+            // settings ok, let's go
+            global.map = new Map("map");
+            global.map.loadContext();
+            startListenToSocket();
+        } else {
+            // no settings, first time run?
+            window.location = "config.html";
+        }
+    });
+
+}());
---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config({silent: true});
+
+var express = require('express');
+var app = express();
+var http = require('http');
+app.use(express.static(__dirname));
+
+httpserver = http.createServer(app);
+
+httpserver.listen(8080, "0.0.0.0", function() {
+    var addr = httpserver.address();
+    console.log("Server listening at ", addr.address + ":" + addr.port);
+});
