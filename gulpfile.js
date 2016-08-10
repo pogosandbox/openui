7270167890b420e114ff543f4df1a4c1147ce2f5
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-08-10 08:33:19 +0200

    add a gulp task to minify js and convert to es2015

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,17 @@
 var gulp        = require('gulp');
-// var gutil       = require('gulp-util');
-// var bowerMain   = require('bower-main');
-// var del         = require('del');
-// var concat      = require("gulp-concat");
-// var uglify      = require("gulp-uglify");
-// var rename      = require("gulp-rename");
+var uglify      = require("gulp-uglify");
+var rename      = require("gulp-rename");
 var sass        = require("gulp-sass");
-// var cssnano     = require("gulp-cssnano");
-// var babel       = require("gulp-babel");
-// var usemin      = require('gulp-usemin');
+var cssnano     = require("gulp-cssnano");
+var babel       = require("gulp-babel");
+var usemin      = require('gulp-usemin');
 
 // Auto
 
 gulp.task('sass', function() {
     return gulp.src('assets/css/*.scss')
                 .pipe(sass.sync().on('error', sass.logError))
-                //.pipe(cssnano())
+                .pipe(cssnano())
                 .pipe(gulp.dest(function(f) {
                     return f.base;
                 }));
@@ -33,18 +29,21 @@ gulp.task('default', [ "sass" ]);
 
 // Prod
 
-// gulp.task("replace-html", function() {
-//     return gulp.src("client/index.html")
-//                 .pipe(usemin({
-//                     js: [
-//                         babel({ presets: ['es2015'] }),
-//                         uglify()
-//                     ],
-//                     libjs: [
-//                         uglify({ mangle: false, compress: false })
-//                     ]
-//                 }))
-//                 .pipe(gulp.dest("client/"));
-// });
+gulp.task("indexhtml", function() {
+    return gulp.src("index.htm")
+           .pipe(rename("index.html"))
+           .pipe(gulp.dest("."));
+});
 
-// gulp.task("prod", [ "replace-html" ]);
+gulp.task("replace-html", ["indexhtml"], function() {
+    return gulp.src("index.html")
+                .pipe(usemin({
+                    js: [
+                        babel({ presets: ['es2015'] }),
+                        uglify()
+                    ]
+                }))
+                .pipe(gulp.dest("."));
+});
+
+gulp.task("prod", [ "sass", "indexhtml", "replace-html" ]);
